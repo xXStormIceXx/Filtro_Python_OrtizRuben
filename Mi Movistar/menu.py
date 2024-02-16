194fcececc7c65a10a7475f6913ac561431edd37
Ruben Ortiz <ruben.ortiz.db9@gmail.com>
2024-02-16 09:25:53 -0500

    aaaaa

--- Mi Movistar/menu.py
+++ Mi Movistar/menu.py
@@ -23,13 +23,27 @@ while (contador != 6):
     if (respuesta == 2):
         print("=============================", "\n", "* PRODUCTOS Y SERVICIOS *", "\n","==================================")
         print("1. Fibra Optica","\n", "2. Planes Pospago", "\n", "3. Planes Prepago", "\n", "4. Telefonia Hogar + TV Full", "\n", "5. Telefonia, TV Full", "\n", "0. Menu de Inicio")
+        ingreso2 = int(input("Selecciona una Opcion (0-5)"))
+        if (ingreso2 == 1):
+            file.crear_usuario()
+            file.limpiar_pantalla
+        if (ingreso2 == 2):
+            file.actualizar_tipo_Usuario()
+            file.limpiar_pantalla
+        if (ingreso2 == 3):
+        if (ingreso2 == 4):
+        if (ingreso2 == 5):
+        if (ingreso2 == 0):
+            input("")
     file.limpiar_pantalla
     if (respuesta == 3):
         print("=============================", "\n", "*    PROMOCIONES    *", "\n","==================================")
         print("1. Combo Personal","\n", "2. Combo Familiar","\n","0. Menu de Inicio")
+        ingreso3 = int(input("Selecciona una Opcion (0-2)"))
     file.limpiar_pantalla
     if (respuesta == 4):           
         print("=============================", "\n", "*  INFORMES/REPORTES  *", "\n","==================================")
         print("1. Lista Productos y Servicios","\n", "2. Productos y Servicios mas Usados", "\n", "3. Lista de Productos y Servicios Por Ususario","\n","0. Menu de Inicio")
+        ingreso4 = int(input("Selecciona una Opcion (0-3)"))
     if (respuesta == 0):
         break
