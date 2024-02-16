dee7208c4fc4f631891c1d724f1930b8d34d0c97
Ruben Ortiz <ruben.ortiz.db9@gmail.com>
2024-02-16 09:02:13 -0500

    puliendo menu y aplicando funciones

--- Mi Movistar/MovistarCRUD.py
+++ Mi Movistar/MovistarCRUD.py
@@ -7,3 +7,130 @@ def limpiar_pantalla():
     else:
         os.system('clear')
 
+def cargar_datos(ruta):
+    try:
+        fd = open(ruta, "r")
+    except Exception as e:
+        try:
+            fd = open(ruta, "w")
+        except Exception as d:
+            print("Error al intentar abrir el archivo\n", d)
+            input("Presione cualquier tecla para continuar\n")
+            return None
+    try:
+        linea = fd.readline()
+        if linea.strip() != "":
+            fd.seek(0)
+            lstPersonal = json.load(fd)
+        else:
+            lstPersonal = []
+    except Exception as e:
+        print("Error al cargar la información\n", e)
+        input("Presione cualquier tecla para continuar\n")
+        return None
+        
+    try:
+        if not fd.closed:
+            fd.close()
+    except Exception as e:
+        print("Error al cerrar el archivo.\n", e, "\n")
+        input("Presione cualquier tecla para continuar\n")
+        return None
+    return lstPersonal
+
+def cargar_datos(ruta):
+    try:
+        fd = open(ruta, "r")
+    except Exception as e:
+        try:
+            fd = open(ruta, "w")
+        except Exception as d:
+            print("Error al intentar abrir el archivo\n", d)
+            input("Presione cualquier tecla para continuar\n")
+            return None
+    try:
+        linea = fd.readline()
+        if linea.strip() != "":
+            fd.seek(0)
+            lstPersonal = json.load(fd)
+        else:
+            lstPersonal = []
+    except Exception as e:
+        print("Error al cargar la información\n", e)
+        input("Presione cualquier tecla para continuar\n")
+        return None
+        
+    try:
+        if not fd.closed:
+            fd.close()
+    except Exception as e:
+        print("Error al cerrar el archivo.\n", e, "\n")
+        input("Presione cualquier tecla para continuar\n")
+        return None
+    return lstPersonal
+
+def guardar_datos(lstPersonal, ruta):
+    # Función que guarda los datos de la lista de personal
+    # en un arcivo JSON
+    # Devuelve True: si los datos fueron guardados correctamente
+    # Devuelve False: si los datos no se pudieron guardar
+    try:
+        fd = open(ruta, "w")
+    except Exception as e:
+        print("Error al abrir el archivo para guardar al empleado.\n", e)
+        input("Presione cualquier tecla para continuar\n")
+        return False
+    
+    try:
+        json.dump(lstPersonal, fd, indent=4)
+    except Exception as e:
+        print("Error al guardar la información del empleado.\n", e)
+        input("Presione cualquier tecla para continuar\n")
+        return False
+    
+    try:
+        if not fd.closed:
+            fd.close()
+    except Exception as e:
+        print("Error al cerrar el archivo.")
+        input("Presione cualquier tecla para continuar\n")
+        return False
+    
+    return True
+
+def crear_camper():
+    identificacion = str(input("ingresa el numero de identificación: ")) #Al inicio debes indicar donde quieres ingresar la informacion en  el JSON
+    nombre = str(input(" ingresa el nombre: "))
+    apellido1 = str(input(" ingresa el primer apellido: "))
+    apellido2 = str(input(" ingresa el segundo apellido: "))
+    direccion = str(input(" ingresa la direccion del camper: "))
+    telefono1 = str(input(" ingresa el telefono celular: "))
+    telefono2 = str(input(" ingresa el telefono fijo: "))
+    edad = str(input(" ingresa la edad: "))
+    estado = str(input(" ingrese el estado actual: "))
+    acudiente = str(input(" ingresa el acudiente: "))
+    data = cargar_datos("proyecto_python/data.json")
+    id = len(data) + 1;
+    nuevo_objeto = {"id": id, 
+                    'identificacion':identificacion,
+                    'nombre':nombre,
+                    'apellido1':apellido1,
+                    'apellido2':apellido2,
+                    'direccion':direccion,
+                    'telefono1':telefono1,
+                    'telefono2':telefono2,
+                    'edad':edad,
+                    'estado':estado,
+                    'acudiente':acudiente}
+    data.append(nuevo_objeto)
+    guardar_datos(data, 'proyecto_python/data.json')
+
+def leer_camper():
+    with open('datos.json', 'r') as archivo:
+        data = json.load(archivo)
+    if data:
+        print("Lista de Campers:")
+        for camper in data:
+            print(f"Identificacion:{camper['identificacion']}, Nombre: {camper['nombre']}, Apellido 1:{camper['apellido1']}, Apellido2:{camper['apellido2']}, Direccion:{camper['direccion']}, Telefono1:{camper['telefono1']}, Telefono2:{camper['telefono2']}, Edad: {camper['edad']}, Estado: {camper['estado']}")
+    else:
+        print("No hay Campers para mostrar.")
--- Mi Movistar/menu.py
+++ Mi Movistar/menu.py
@@ -9,14 +9,18 @@ while (contador != 6):
     file.limpiar_pantalla
     if (respuesta == 1):
         print(" ============================", "\n", "*      USUARIOS      *", "\n","==================================")
+        print("1. Crear Nuevo Usuario","\n", "2. Asignar Categoria Usuario")
     file.limpiar_pantalla
     if (respuesta == 2):
         print("=============================", "\n", "* PRODUCTOS Y SERVICIOS *", "\n","==================================")
+        print("1. Fibra Optica","\n", "2. Planes Pospago", "\n", "3. Planes Prepago", "\n", "4. Telefonia Hogar + TV Full", "\n", "5. Telefonia, TV Full", "\n", "0. Menu de Inicio")
     file.limpiar_pantalla
     if (respuesta == 3):
         print("=============================", "\n", "*    PROMOCIONES    *", "\n","==================================")
+        print("1. Combo Personal","\n", "2. Combo Familiar")
     file.limpiar_pantalla
     if (respuesta == 4):           
         print("=============================", "\n", "*  INFORMES/REPORTES  *", "\n","==================================")
+        print("1. Lista Productos y Servicios","\n", "2. Productos y Servicios mas Usados", "\n", "3. ")
     if (respuesta == 0):
         break
