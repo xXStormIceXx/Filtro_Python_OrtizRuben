fd219c5f4ca19aadcd4ca419c8b56be1fff3413c
Ruben Ortiz <ruben.ortiz.db9@gmail.com>
2024-02-16 09:52:12 -0500

    actualizacion 5

--- Mi Movistar/menu.py
+++ Mi Movistar/menu.py
@@ -26,21 +26,45 @@ while (contador != 6):
         print("1. Fibra Optica","\n", "2. Planes Pospago", "\n", "3. Planes Prepago", "\n", "4. Telefonia Hogar + TV Full", "\n", "5. Telefonia, TV Full", "\n", "0. Menu de Inicio")
         ingreso2 = int(input("Selecciona una Opcion (0-5)"))
         if (ingreso2 == 1):
-            file.crear_usuario()
-            file.limpiar_pantalla
+          script = os.path.dirname(__file__)
+          with open(script+"/planes.json.json") as file:
+            data = json.load(file)
+            for client in data["Planes"]:
+                print("Plan 1", client["Plan 1"])
+                print("Precio", client["precio"])
+                print("")
         if (ingreso2 == 2):
-            file.actualizar_tipo_Usuario()
-            file.limpiar_pantalla
+          script = os.path.dirname(__file__)
+          with open(script+"/planes.json.json") as file:
+            data = json.load(file)
+            for client in data["Planes"]:
+                print("Plan 2", client["Plan 2"])
+                print("Precio", client["precio"])
+                print("")
         if (ingreso2 == 3):
           script = os.path.dirname(__file__)
           with open(script+"/planes.json.json") as file:
             data = json.load(file)
             for client in data["Planes"]:
-                print("Nombre", client["nombre"])
-                print("Apellido", client["apellido"])
+                print("Plan 3", client["Plan 3"])
+                print("Precio", client["precio"])
                 print("")
         if (ingreso2 == 4):
+          script = os.path.dirname(__file__)
+          with open(script+"/planes.json.json") as file:
+            data = json.load(file)
+            for client in data["Planes"]:
+                print("Plan 4", client["Plan 4"])
+                print("Precio", client["precio"])
+                print("")
         if (ingreso2 == 5):
+          script = os.path.dirname(__file__)
+          with open(script+"/planes.json.json") as file:
+            data = json.load(file)
+            for client in data["Planes"]:
+                print("Plan 5", client["Plan 5"])
+                print("Precio", client["precio"])
+                print("")
         if (ingreso2 == 0):
             input("")
     file.limpiar_pantalla
